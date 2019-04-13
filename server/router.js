5c313529a7d15f2aa64ca2ffe79a24d0b6844269
Tracy Yu <tracyyu93@g.ucla.edu>
2019-04-12 17:28:28 -0700

    Update to fix folder structure and css stylings

--- server/router.js
+++ server/router.js
@@ -3,10 +3,27 @@ const controller = require('./controller.js');
 
 router
     .route('/')
-    .get(controller.getAll);
+    .get(controller.getAllReservations)
+    .post(controller.createReservation)
+    .delete(controller.deleteAllReservations)
 
 router
     .route('/:productID')
+    .get(controller.getReservationByProductID)
+    .delete(controller.deleteReservationByProductID)
+    .put(controller.updateReservationByProductID)
+
+
+router
+    .route('/shop/designers')
+    .get(controller.getAll);
+
+router
+    .route('/shop/designers')
+    .get(controller.getAll);
+
+router
+    .route('/shop/designers/:productID')
     .get(controller.getProductReviews);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,15 +3,32 @@ const path = require('path');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const routes = require('./router.js');
+const request = require('request');
+var rp = require('request-promise');
+// const routes = require('./router.js');
 
 const app = express();
 const port = 3000;
+
+const router = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../dist')));
-app.use('/api/shop/designers', routes);
+
+app.all('/api/*', (req,res) => {
+    var pathArr = req.path.split('/');
+    if(pathArr[2] === 'reviews'){
+        res.redirect('http://localhost:3002' + req.path);
+    } else if(pathArr[2] === 'images') {
+        res.redirect('http://localhost:1143' + req.path);
+    } else if(pathArr[2] === 'gallery') {
+        res.redirect('http://localhost:3005' + req.path);
+    } else {
+        res.redirect('http://localhost:3001' + req.path);
+    }
+})
+
 
 app.listen(port, () => `Listening for connections on port ${port}`);
